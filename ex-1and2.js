0da69aae229cf31c9c1ce2c4d7cfe506dbf6149f
Bura <bu_bangplee@hotmail.com>
2024-05-27 10:40:07 +0700

    feat:submit ex-1-2-3-4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = 'Karter';
+let lastName = "Anderson";
+let addressPerson = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const nameStore = "Chef's Table by Chef James";
+const billStore = 15000;
+const totalStore = 1500;
+
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+addressPerson = '(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641';
+//billStore = 5000;
+//totalStore = 2500;
